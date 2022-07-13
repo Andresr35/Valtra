2528ff2fa5cd03bae9b476a48a40c0f828f9b426
Luis A <46586879+arizmendi-luis@users.noreply.github.com>
2022-07-13 14:26:58 -0700

    byeserver

--- server/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-
-var router = express.Router();  
-
-router.use("/",require("./products"));
-router.use("/shopify",require("./shopify"));
-//router.use('/pdf',require('./pdf'));
-
-module.exports = router;
