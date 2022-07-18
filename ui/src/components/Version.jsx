0441016158d596d0e9718ab173239d51e357a025
Luis A <46586879+arizmendi-luis@users.noreply.github.com>
2022-07-18 11:43:10 -0700

    backtonotlocal

--- ui/src/components/Version.jsx
+++ ui/src/components/Version.jsx
@@ -15,7 +15,7 @@ export const Version = () => {
     );
 }  
 //true for local host testing and false for azure deploy 
-export const isLocal = true;
+export const isLocal = false;
 
 //changes automatically to port 
 const port = process.env.PORT || 3005; 
