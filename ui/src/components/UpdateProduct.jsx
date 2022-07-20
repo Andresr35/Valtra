d5320235efef50b1555da3e4b6d9fa0be9568461
Luis A <46586879+arizmendi-luis@users.noreply.github.com>
2022-07-20 11:41:37 -0700

    1.1.12

--- ui/src/components/UpdateProduct.jsx
+++ ui/src/components/UpdateProduct.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 //import { useContext } from 'react';
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom' 
+import Container from "react-bootstrap/Container"; 
 //import { ProductsContext } from '../context/ProductsContext';
 import { useEffect } from 'react';
 import ProductFinder from '../api/ProductFinder';
@@ -11,14 +12,14 @@ const UpdateProduct = (props) => {
    //const {products} = useContext(ProductsContext)
    const {id} = useParams();
    const[ids,setID] = useState("");
-   const[description,setDescription] = useState("");
+   const[description,setDescription] = useState(""); 
 
    useEffect( () => {
     const fetchData = async() => {
         try{
             const response = await ProductFinder.get(`/${id}`);
             setID(response.data.data.products.id);
-            setDescription(response.data.data.products.description);
+            setDescription(response.data.data.products.description); 
         }catch(err){}
     };
     fetchData(); 
@@ -33,23 +34,26 @@ const UpdateProduct = (props) => {
     }
 
   return (
-    <div> 
-        
+    <div>  
+      <Container>
       <form action="">
           {/* This is the form part for changing the id */}
           <div className="form-group">
-              <label htmlFor="id">ID</label>
-              <input value = {ids} onChange = {e => setID(e.target.value)} type="text" className="form-control" id = "id" />
+              <label style={{marginLeft: '.7rem', marginRight: '.7rem'}} htmlFor="id">ID</label>
+              <input style={{marginLeft: '.7rem', marginRight: '.7rem'}} value = {ids} onChange = {e => setID(e.target.value)} type="text" className="form-control" id = "id" />
           </div>
-        {/* this is the form part for the description */}
+        {/* this is the form part for the description */} 
+        
           <div className="form-group">
-              <label htmlFor="description">Description</label>
-              <input value = {description} onChange = {e => setDescription(e.target.value)} type="text" className="form-control" id = "description" />
+              <label style={{marginLeft: '.7rem', marginRight: '.7rem'}} htmlFor="description">Description</label>
+              <input style={{marginLeft: '.7rem', marginRight: '.7rem'}} value = {description} onChange = {e => setDescription(e.target.value)} type="text" className="form-control" id = "description" />
           </div>
-          <button type='submit' onClick={handleSubmit} className="btn btn-primary">Update</button>
-      </form>
-    </div>
+          <button style={{marginLeft: '.7rem', marginRight: '.7rem'}} type='submit' onClick={handleSubmit} className="btn btn-primary">Update</button>
+      </form> 
+      </Container>
+    </div> 
   )
-}
+} 
 
-export default UpdateProduct
+
+export default UpdateProduct 
